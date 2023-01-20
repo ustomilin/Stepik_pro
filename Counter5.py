923fca897fd3b02d043d5e8f84febbde703e8c68
ustomilin <ustomilin@gmail.com>
2023-01-19 22:40:47 -0800

    Price by ord

---
+++ Counter5.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+def count_sym(file_name):
+    with open(file_name, encoding='utf8') as f:
+        f = f.read().lower()
+        count = Counter(f)
+        for sym, kol in sorted(count.items()):
+            if sym.isalpha():
+                print(f'{sym}: {kol}')
+
+count_sym('pythonzen.txt')
