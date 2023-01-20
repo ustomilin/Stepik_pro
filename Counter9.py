8dffa039b87b7d71d12f9ae5d9d52bfdf3d81888
ustomilin <ustomilin@gmail.com>
2023-01-20 11:47:14 -0800

    count log strings

--- Counter9.py
+++ Counter9.py
@@ -1,6 +1,10 @@
 from collections import Counter
 import csv
 
-with open('/files/name_log.csv', encoding='utf8') as file:
-    text = csv.reader(file)
-    print(text)
+with open('files/name_log.csv', encoding='utf8') as file:
+    text = list(csv.reader(file))
+    del text[0]
+    text = list(map(lambda x: x[1], text))
+    c = Counter(text)
+    for email, count in sorted(c.items()):
+        print(f'{email}: {count}')
