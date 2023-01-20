1f983bf867cfc31bda4441eff544a6ad3bde4429
ustomilin <ustomilin@gmail.com>
2023-01-20 13:18:35 -0800

    books prices

--- Counter13.py
+++ Counter13.py
@@ -1,11 +1,11 @@
 from collections import Counter
 
 books = Counter(map(int, input().split()))
-su = 0
-for i in range(int(input())):
+total = 0
+for _ in range(int(input())):
     uch = list(map(int, input().split()))
     if uch[0] in books.keys() and books[uch[0]] > 0:
-        su += uch[1]
+        total += uch[1]
         books[uch[0]] -= 1
 
-print(su)
+print(total)
