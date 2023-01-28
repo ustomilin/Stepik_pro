0d106ad3d7cc9845402938f013c2fbc292844dae
ustomilin <ustomilin@gmail.com>
2023-01-27 16:28:15 -0800

    Check password EAFP2-3

--- LBYL-EAFP2.py
+++ LBYL-EAFP2.py
@@ -1,3 +1,5 @@
+from sys import stdin
+
 class PasswordError(Exception):
     pass
 
@@ -21,10 +23,48 @@ def is_good_password(string):
         raise LetterError
     if not any(i.isdigit() for i in string):
         raise DigitError
-    return True
+    return 'Success!'
 
 
-try:
-    print(is_good_password('41157081231232'))
-except Exception as err:
-    print(type(err))
+while True:
+    try:
+        print(is_good_password(input()))
+        break
+    except Exception as err:
+        print(err.__class__.__name__)
+        continue
+from sys import stdin
+
+class PasswordError(Exception):
+    pass
+
+
+class LengthError(PasswordError):
+    pass
+
+
+class LetterError(PasswordError):
+    pass
+
+
+class DigitError(PasswordError):
+    pass
+
+
+def is_good_password(string):
+    if len(string) < 9:
+        raise LengthError
+    if not any(i.islower() for i in string) or not any(i.isupper() for i in string):
+        raise LetterError
+    if not any(i.isdigit() for i in string):
+        raise DigitError
+    return 'Success!'
+
+
+while True:
+    try:
+        print(is_good_password(input()))
+        break
+    except Exception as err:
+        print(err.__class__.__name__)
+        continue
